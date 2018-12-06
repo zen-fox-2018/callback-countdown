ebf552d6c328a55b0448a3819f0be98f2eb4543f
ShadowM8 <anton2014lenovo@gmail.com>
2018-12-06 17:21:33 +0700

    cb-countdown -anton-

--- controllers.js
+++ controllers.js
@@ -1,7 +1,29 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  // Your code here...  
+  let something = setInterval(function(){
+    seconds--
+    let menit = ~~(seconds/60)
+    if(menit <10){
+      menit = `0${menit}`
+    }
+    let detik = seconds % 60
+    if (detik < 10){
+      detik = `0${detik}`
+    }
+    if(seconds >= 0){
+      displayCountdown(`${menit}:${detik}`)
+    }    
+    else{
+      clearInterval(something)
+    }
+    
+
+    // cb(seconds)
+  }, 100)
+
+  
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -5,7 +5,7 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  console.log(chalk.cyan(figlet.textSync(seconds)))
 };
 
 module.exports = { displayCountdown };
