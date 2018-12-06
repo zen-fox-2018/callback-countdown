d7368976f906496a42ddbaa1b4c7f18d31334349
ChristianHS23 <christian.sihotang23@gmail.com>
2018-12-06 16:47:11 +0700

    callback coundown

--- controllers.js
+++ controllers.js
@@ -2,6 +2,23 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let countdown = setInterval(()=>{
+    let minute = ~~(seconds/60)
+    if(minute < 10){
+      minute = 0+String(minute)
+    }
+    curseconds = seconds % 60 
+    if(curseconds < 10){
+      curseconds = 0 + String(curseconds)
+    }
+    console.log(curseconds)
+    if(seconds <= 0){
+      clearInterval(countdown)
+    }
+    displayCountdown(minute,curseconds)
+    seconds--
+  },1000)
+  
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -2,9 +2,12 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
-const displayCountdown = seconds => {
+const displayCountdown = (minutes,seconds) => {
   clear();
-  console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.red(figlet.textSync("Countdown App")));
+ 
+  console.log(chalk.blue(figlet.textSync(`${minutes} : ${seconds}`)));
+  
   // Your code here...
 };
 
