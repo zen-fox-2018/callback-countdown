03c07c7f81ce45f4b8ff186751cf528755ef3fd6
Aditya Respati <AdityaRespati>
2018-12-06 18:34:00 +0700

    commit

--- controllers.js
+++ controllers.js
@@ -1,8 +1,40 @@
-const { displayCountdown } = require("./views");
+const { displayCountdown } = require("./views.js");
 
 const timer = seconds => {
   // Your code here...
+
+  var Interval = setInterval(function(){
+    var MMDD = ''
+    var Min = Math.floor(seconds / 60 )
+    var sec = seconds % 60
+
+    if(Min < 10){
+      MMDD += '0' + Min
+    } 
+    else {
+      MMDD += Min
+    }
+
+    MMDD += ' : '
+
+    if(sec < 10){
+      MMDD += '0' + Min
+    }
+    else {
+      MMDD += sec
+    }
+
+    if(seconds >=0 ){
+      seconds--
+      displayCountdown(MMDD)
+    } 
+    else {
+      clearInterval(Interval)
+    }
+  }, 300)
 };
+
+
 
 module.exports = {
   timer
--- views.js
+++ views.js
@@ -5,6 +5,7 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.cyan(figlet.textSync(seconds)))
   // Your code here...
 };
 
