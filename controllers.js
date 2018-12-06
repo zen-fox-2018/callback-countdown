dbe3488e66de009eac2cb353c69ada61d5267bb3
ACER <fauziaazia171097@gmail.com>
2018-12-06 04:29:53 -0500

    Finish

--- controllers.js
+++ controllers.js
@@ -1,6 +1,15 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
+  let time = seconds
+  let inter = setInterval(() => {
+      if (time >= 0) {
+        displayCountdown(time)
+        time--
+      } else if (time < 0){
+        clearInterval(inter)
+      }
+  },500)
   // Your code here...
 };
 
--- views.js
+++ views.js
@@ -5,6 +5,24 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  var jam = Math.floor((seconds / 3600));
+  var menit = Math.floor(((seconds % 3600) / 60));
+  var detik = Math.floor(seconds % 60);
+
+  if ( jam < 10) {
+    jam = "0" + jam
+  }
+  if ( menit < 10 ) {
+    menit = '0' + menit;
+  }
+  if ( detik < 10) {
+    detik = '0' + detik;
+  }
+
+// Return display.
+  let print =  jam + " : " + menit + ' : ' + detik;
+
+  console.log(chalk.cyan(figlet.textSync(print)));
   // Your code here...
 };
 
