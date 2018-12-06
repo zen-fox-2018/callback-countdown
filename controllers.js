93401de91cd0a6cecd73a7ea039a05798e37bb3e
fauzan_bintang <obin.aji@gmail.com>
2018-12-06 15:31:40 +0700

    done

--- controllers.js
+++ controllers.js
@@ -1,7 +1,19 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let timer = setInterval(() => {
+    let result = ''
+    let menit = Math.floor(seconds/60)
+    let detik = seconds%60
+    menit < 10 ? result += '0' + menit: result += menit
+    result += ':'
+    detik < 10 ? result += '0' + detik: result += detik
+    displayCountdown(result)
+    seconds--
+    if (seconds < 0) {
+      clearInterval(timer)
+    }
+  }, 1000)
 };
 
 module.exports = {
