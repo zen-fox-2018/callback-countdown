35585dc94b2f11a0462f1b494b6885254e8f8240
LuthfiArfi <luthfi.arfi@gmail.com>
2018-12-06 17:06:09 +0700

    bug fix

--- views.js
+++ views.js
@@ -6,7 +6,21 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
-  console.log(chalk.cyan(figlet.textSync(seconds)));
+  var minuteinView = 0;
+  while (seconds > 60) {
+    seconds -= 60;
+    minuteinView += 1;
+  }
+  var secondinView = seconds;
+  minuteinView = String(minuteinView);
+  secondinView = String(secondinView);
+  if (minuteinView.length == 1) {
+    minuteinView = '0'+minuteinView;
+  }
+  if (secondinView.length == 1) {
+    secondinView = '0'+secondinView;
+  }
+  console.log(chalk.cyan(figlet.textSync(minuteinView+' : '+secondinView)));
 };
 
 module.exports = { displayCountdown };
