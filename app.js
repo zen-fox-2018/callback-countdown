004c0a75bfab6f418b0a36e59471bed43ef90ef8
Januarsmad <januarsmad@gmail.com>
2018-12-06 16:22:07 +0700

    done

--- app.js
+++ app.js
@@ -6,3 +6,4 @@ const main = () => {
 };
 
 main();
+
--- controllers.js
+++ controllers.js
@@ -2,8 +2,28 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  let timer = setInterval(function(){
+    let min = Math.floor(seconds/60)
+    let sec = seconds % 60
+    let display = ''
+    
+    if(min < 10) {
+      min = `0${min}`
+    }
+    if(sec < 10) {
+      sec = `0${sec}`
+    }
+    display = `${min} : ${sec}`
+    if(seconds === 0){
+      clearInterval(timer)
+    }
+    // console.log(`${min} : ${seconds}`)
+    displayCountdown(display)
+    seconds-- 
+  }, 1000);
 };
 
 module.exports = {
   timer
 };
+
