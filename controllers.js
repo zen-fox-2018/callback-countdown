1c8a9dfb22650b5d1833cbec4b7b7ac28d847ec1
Celyn Susanto <celyn.vy@gmail.com>
2018-12-06 17:19:36 +0700

    done

--- controllers.js
+++ controllers.js
@@ -2,6 +2,14 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+    let interval = setInterval(function() {
+      if (seconds >0) {
+        seconds -= 1;
+        displayCountdown(seconds);
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -6,6 +6,26 @@ const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
   // Your code here...
+  //change seconds to MM:DD
+  let second = seconds % 60;
+  let minutes = (seconds - second) / 60
+  let theSecond = 0;
+  let theMins = 0;
+  if (second < 10) {
+    theSecond = `0${second}`
+  } else {
+    theSecond = second
+  }
+  if (minutes < 10) {
+    theMins = `0${minutes}`
+  } else {
+    theMins = minutes;
+  }
+  if (second === 0 && minutes != 0) {
+    minutes -= 1
+  }
+  let time = `${theMins} : ${theSecond}`
+  console.log(chalk.cyan(figlet.textSync(time)));
 };
 
 module.exports = { displayCountdown };
