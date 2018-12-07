99afa3e23ee5113f7625cb89e26c31e6fbb239fb
rahmanhasri <rahman.hasri@gmail.com>
2018-12-07 10:24:42 +0700

    lupa push

--- controllers.js
+++ controllers.js
@@ -1,7 +1,27 @@
-const { displayCountdown } = require("./views");
+const { displayCountdown, error } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+
+  if (seconds > 3599) {
+    error()
+  } else {
+    let interval = setInterval(function(){
+      seconds--
+      let minutes = Math.floor(seconds / 60);
+      let detiks = seconds % 60
+      if (detiks < 10) {
+        detiks = '0' + detiks
+      }
+      if (minutes < 10) {
+        minutes = '0' + minutes
+      }
+      displayCountdown(minutes ,detiks)
+      if (!seconds) {
+        clearInterval(interval)
+      }
+    }, 1000)
+  }
 };
 
 module.exports = {
--- views.js
+++ views.js
@@ -2,10 +2,16 @@ const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
-const displayCountdown = seconds => {
+const displayCountdown = (minutes, seconds) => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.yellow(figlet.textSync(`${minutes} : ${seconds}`)));
   // Your code here...
 };
 
-module.exports = { displayCountdown };
+const error = () => {
+  console.log(chalk.red(figlet.textSync("Your input is more than")));
+  console.log(chalk.red(figlet.textSync("1    Hour")));
+}
+
+module.exports = { displayCountdown, error };
